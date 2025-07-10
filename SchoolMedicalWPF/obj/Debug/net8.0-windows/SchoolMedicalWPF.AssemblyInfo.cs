0a7419bfde62fcc832e7e67a2b8f8b0578039516
lnam22871 <lnam22871@gmail.com>
2025-07-10 11:29:38 +0700

    ok

--- SchoolMedicalWPF/obj/Debug/net8.0-windows/SchoolMedicalWPF.AssemblyInfo.cs
+++ SchoolMedicalWPF/obj/Debug/net8.0-windows/SchoolMedicalWPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolMedicalWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3692c2cb49df5ae3e304118a2f3837487ffc68b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5935d8b6fa6895b5e123e71da1a72141f03fc437")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolMedicalWPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolMedicalWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
